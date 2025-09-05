43fd55a5602697e3ca6165f421d9caf167d85a12
TonmoyZohani <tonmoyzohani@gmail.com>
2025-09-05 16:22:26 +0600

    users shown

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,23 @@ function App() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/users`)
+    fetch("http://localhost:5000/users")
       .then((res) => res.json())
       .then((data) => setUsers(data));
   }, []);
 
   return (
     <div>
-      {" "}
-      <h2>Users Management System</h2> <br></br> Numbers of Users:{" "}
-      {users.length}
+      <h2>Users Management System</h2>
+      <p>Numbers of Users: {users.length}</p>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <strong>{user.name}</strong> — {user.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
