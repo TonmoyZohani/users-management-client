fcaaaa49d15184d2fde3b2145c2a5b9e4fe9fcfe
TonmoyZohani <tonmoyzohani@gmail.com>
2025-09-05 16:13:58 +0600

    trying to get users

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import "./index.css";
 
 function App() {
   const [users, setUsers] = useState([]);
 
-  return <></>;
+  useEffect(() => {
+    fetch(`http://localhost:5000/users`)
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      {" "}
+      <h2>Users Management System</h2> <br></br> Numbers of Users:{" "}
+      {users.length}
+    </div>
+  );
 }
 
 export default App;
